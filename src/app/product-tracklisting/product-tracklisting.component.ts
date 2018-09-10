0104b17168272f8367855e0fbe93e5cde2514ad8
Patrick Congdon <prcongdon@hotmail.com>
2018-09-10 18:39:03 -0400

    Work for part 6.

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,7 +9,7 @@ import {ProductService} from '../product.service';
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  albumInfo:Album;
+  albumInfo: Album;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
